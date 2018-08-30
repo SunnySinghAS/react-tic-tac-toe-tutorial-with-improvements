9a91b57952b99335a3843e951c2f0ce5941f6c02
Sunny Singh <rc_368@hotmail.com>
2018-08-30 14:05:43 -0700

    Cleaned up Majority ESLint errors and split classes

--- src/index.js
+++ src/index.js
@@ -1,43 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Board from './Board';
 
-function Square(props) {
-  return (
-    <button className="square" onClick={props.onClick}>
-      {props.value}
-    </button>
-  );
-}
-
-class Board extends React.Component {
-  renderSquare(i) {
-    return (
-      <Square value={this.props.squares[i]} onClick={() => this.props.onClick(i)} />
-    );
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let index = 0; index < lines.length; index += 1) {
+    const [a, b, c] = lines[index];
+    if (squares[a] && (squares[a] === squares[b]) && (squares[b] === squares[c])) {
+      return (squares[a]);
+    }
   }
-
-  render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
+  return (null);
 }
 
 class Game extends React.Component {
@@ -62,7 +45,7 @@ class Game extends React.Component {
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       history: history.concat([{
-        squares: squares,
+        squares,
       }]),
       stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
@@ -109,26 +92,6 @@ class Game extends React.Component {
       </div>
     );
   }
-}
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let index = 0; index < lines.length; index += 1) {
-    const [a, b, c] = lines[index];
-    if (squares[a] && (squares[a] === squares[b]) && (squares[b] === squares[c])) {
-      return (squares[a]);
-    }
-  }
-  return (null);
 }
 
 // ========================================
